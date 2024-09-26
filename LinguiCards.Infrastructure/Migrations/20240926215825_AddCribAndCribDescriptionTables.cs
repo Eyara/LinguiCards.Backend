0089060a173499b8fbf9b51c6cdce1f0760cdec8
Andrei Sakharov <andrei.saharoff3011@gmail.com>
2024-09-27 01:07:45 +0300

    migrations for cribs

---
+++ LinguiCards.Infrastructure/Migrations/20240926215825_AddCribAndCribDescriptionTables.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LinguiCards.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCribAndCribDescriptionTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CribDescriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Header = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Order = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    CribId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CribDescriptions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DefaultCribs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LanguageId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DefaultCribs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DefaultCribs_Languages_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Languages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DefaultCribs_LanguageId",
+                table: "DefaultCribs",
+                column: "LanguageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CribDescriptions");
+
+            migrationBuilder.DropTable(
+                name: "DefaultCribs");
+        }
+    }
+}
