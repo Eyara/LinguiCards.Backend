6be41af0e21378beea531a7ebfe2d427d6cabf42
Andrei Sakharov <andrei.saharoff3011@gmail.com>
2024-10-16 15:15:38 +0300

    missed files

---
+++ LinguiCards.Infrastructure/Migrations/20241016121125_AddedTrainingIdFieldWordHistoriesTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinguiCards.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTrainingIdFieldWordHistoriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Answer",
+                table: "WordChangeHistories",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TrainingId",
+                table: "WordChangeHistories",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Answer",
+                table: "WordChangeHistories");
+
+            migrationBuilder.DropColumn(
+                name: "TrainingId",
+                table: "WordChangeHistories");
+        }
+    }
+}
